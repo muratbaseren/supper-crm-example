3045941dbba790a22de6f6192304a777331c9401
Murat Başeren <muratbaseren@gmail.com>
2022-01-28 18:50:43 +0300

    S24_E29 : Müşteri Sayfası Html Refactoring

---
+++ SupperCRMExample.WebApp/Models/ModalInputViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SupperCRMExample.WebApp.Models
+{
+    public class ModalInputViewModel
+    {
+        public bool IsReadonly { get; set; } = false;
+        public string Description { get; set; } = "Tincidunt integer eu augue augue nunc elit dolor, luctus placerat scelerisque euismod.";
+        public bool HasClientId { get; set; } = false;
+    }
+}
